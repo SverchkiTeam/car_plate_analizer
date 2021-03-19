3d065b5080e5cfbf0d0f3256739e622457777276
Heorhii Yatsenk <h_yatsenko-itssv@kw.com>
2021-03-19 19:02:17 +0200

    added todo

--- api/resources/analyzer_resource.py
+++ api/resources/analyzer_resource.py
@@ -10,6 +10,7 @@ from api.domain.plate_analyzer import PlateAnalyzer
 class AnalyzerResource(BaseResource):
 
     def post(self):
+        # TODO add logic for encoding file from json without saving into server
         f = request.files['image']
         analyzer = PlateAnalyzer()
         return {'plate_number': str(analyzer.get_plate_number(f))}
